2d952984e7308d3ecb1478dd1a3cfcfdfa7372db
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-09-07 01:02:30 +0530

    Create 585.sql

---
+++ inner query/585.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select ROUND(sum(tiv_2016), 2) as tiv_2016
+from Insurance I 
+where tiv_2015 in
+(select tiv_2015 from Insurance group by tiv_2015 having count(*) > 1) 
+and pid in 
+(select pid from Insurance group by lat, lon having count(*) = 1);
